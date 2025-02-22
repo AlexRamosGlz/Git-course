52e38152b5ac937a704ead43f65671cc5d0765ff
AlexRamosGlz <joralexfx@gmail.com>
2025-02-21 19:49:39 -0600

    added new notes

---
+++ git-branch.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+##    --  git branch --    ##
+
+-- git branch --
+
+git branch -d <branch_name>  -> elimina la rama
---
+++ git-merge.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+##    -- git merge --   ##
+
+git merge --abort   # el comando hace que se aborte el merge y resetea el working tree alultimo commit
