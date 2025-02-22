632ad797b76b0d3d9534f11d78612633aae66b86
AlexRamosGlz <joralexfx@gmail.com>
2025-02-21 18:19:13 -0600

    Revert "reboot_check.py renombrado a check.py"
    This reverts commit e31c880c6162536b946423787f06b2da00853750.
    
    Rollback reason: el nombre anterior quedaba mejor

--- git-revert.sh
+++ git-revert.sh
@@ -3,7 +3,3 @@
 ##    -- git rever --   ##
 
 git revert HEAD # es usado como rollback, el comando no solo hace undo, sino que hace commit de los cambios del commit malo pero a la inversa
-
-git revert 886e0a1 # hace rollback del commit especificado
-
-
