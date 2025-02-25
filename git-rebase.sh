8b38f249bc6b4709022f90d795174843e8e936f1
AlexRamosGlz <joralexfx@gmail.com>
2025-02-25 15:43:07 -0600

    anotaciones creadas para el comando 'git rebase'

---
+++ git-rebase.sh
@@ -0,0 +1,10 @@
+#!/bin bash
+
+##    -- git rebase --    ##
+# rebasing significa cambier el commit base que es usado para nuestra branch
+git rebase master # rebase es usado como una alternativa a merge, mantiene el historial de commits linear en ves de seprararlos por branches haciendo que se vea mas limpio el codigo
+
+
+git rebase origin/courser-introduction-git-github # se puede aplicar hacer merge de branches locales con remotas
+
+
